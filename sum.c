6ab097482972adcc397b3f5a4f533816cb6032f3
Tagaiming <tahsinahmed555111@gmail.com>
2026-06-04 21:56:08 +0600

    sum.c

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int i,sum=0;
+    for (i=1;i<=10;i++){
+        if(i%2!=0)
+        {
+        sum=sum+i;
+        }
+       
+
+
+    }
+    printf("your sum is:%d",sum);
+
+    return 0;
+}
