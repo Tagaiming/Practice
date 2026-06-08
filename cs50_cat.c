9fd73f41a3d019252253bcf71ee710291039419e
Tagaiming <tahsinahmed555111@gmail.com>
2026-06-08 14:31:44 +0600

    cs50_cat.c

--- cs50_cat.c
+++ cs50_cat.c
@@ -2,10 +2,24 @@
 
 int main()
 {
-int i=0;
-while(i<3){
-    printf("Meow\n");
-    i++;
-}
+
+// 1st type
+// while(i<3){
+//     printf("Meow\n");
+//     i++;
+// }
+// 2nd type
+// int i=3;
+// while(i>0){
+//     printf("meow\n");
+//     i--;
+    
+// }
+// 3rd type
+// for (int i=0;i<3;i++){
+//     printf("Meow\n");
+
+// }
+
 return 0;
 }
