8a8e69c0d3d953d393147cc2d96aaab9950d9c0f
Tagaiming <tahsinahmed555111@gmail.com>
2026-06-12 18:12:03 +0600

    cs50_calculaotr.c

---
+++ cs50_calculator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int x,y,sum;
+printf("What is X?");
+scanf("%i",&x);
+printf("What is Y?");
+scanf("%i",&y);
+sum=x+y;
+printf("Your answer is:%i\n",sum);
+return 0;
+}
